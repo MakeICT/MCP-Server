aa5e9c3befdb5f02a7e87ce2ee63f27d7dc3d9f5
Christian Kindel <iceman81292@gmail.com>
2019-08-08 15:05:45 -0500

    reconfigure settings file to get everything in one place

--- mcp/config.py
+++ mcp/config.py
@@ -1,25 +1,32 @@
 import os
+from pathlib import Path
+import configparser
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+settings = configparser.ConfigParser()
+settings.read(str(Path(basedir).parents[0]) + '/config.ini')
 
 
 class Config:
     # SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    print('sqlite:///' + os.path.join(basedir, 'site.db'))
-    SECRET_KEY = 'you-will-never-guess'
+    # ENV = 'development'
+    # DEBUG = True
+    SECRET_KEY = settings.get('general', 'secret_key')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') \
         or 'sqlite:///' + os.path.join(basedir, 'site.db')
         # 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
+    MAIL_SERVER = settings.get('email', 'mail_server')
+    MAIL_PORT = settings.get('email', 'mail_server_port')
+    MAIL_USERNAME = settings.get('email', 'username')
+    MAIL_PASSWORD = settings.get('email', 'password')
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
     # Shown in email templates and page footers
-    USER_APP_NAME = "Makerspace Control Program"
+    USER_APP_NAME = settings.get('general', 'app_name')
+    USER_EMAIL_SENDER_EMAIL = settings.get('email', 'sender_email')
     USER_ENABLE_EMAIL = True      # Enable email authentication
     USER_ENABLE_USERNAME = True    # Enable username authentication
     USER_REQUIRE_RETYPE_PASSWORD = True
-    USER_EMAIL_SENDER_EMAIL = ""
