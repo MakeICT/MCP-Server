0e92002f75c3ac1adc2962d8e4397b1479804261
Christian Kindel <iceman81292@gmail.com>
2021-04-21 15:23:52 -0500

    add details to task notifications for error descriptions

--- src/mcp/main/tasks.py
+++ src/mcp/main/tasks.py
@@ -5,7 +5,7 @@ from mcp import create_app, db
 from mcp.main.models import Task
 
 
-def set_task_progress(progress, status=None):
+def set_task_progress(progress, status=None, details=None):
     print(f"set task progress: {progress}")
     job = get_current_job()
     if job:
@@ -14,7 +14,8 @@ def set_task_progress(progress, status=None):
         task = Task.query.get(job.get_id())
         task.user.add_notification('task_progress', {'task_id': job.get_id(),
                                                      'progress': progress,
-                                                     'status': status})
+                                                     'status': status,
+                                                     'details': details})
         if progress >= 100:
             task.complete = True
         db.session.commit()
@@ -25,10 +26,13 @@ def run_as_task(func, *args, **kwargs):
     app.app_context().push()
 
     # print("ARGS:", args)
-
+    app.logger.info(f'Adding task {func.__name__} to RQ')
     try:
         func(*args, **kwargs)
         set_task_progress(100)
-    except:
-        set_task_progress(100, 'failed')
-        app.logger.error('Unhandled exception', exc_info=sys.exc_info())
+        app.logger.info(f'Task {func.__name__} completed successfully')
+    except Exception as e:
+        db.session.rollback()
+        m = getattr(e, 'message', repr(e)).encode().decode('unicode_escape')
+        set_task_progress(100, 'failed', m)
+        app.logger.error(f'{func.__name__} failed due to unhandled exception', exc_info=sys.exc_info())
