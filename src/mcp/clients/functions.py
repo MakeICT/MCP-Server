399a78423c80968f5db1e0261217bd41b03594d7
Christian Kindel <iceman81292@gmail.com>
2021-12-26 15:50:30 -0600

    add file logging for card rejections

--- src/mcp/clients/functions.py
+++ src/mcp/clients/functions.py
@@ -1,3 +1,5 @@
+from flask import current_app
+
 from mcp.users.models import User
 from mcp.clients.models import Client
 
@@ -26,6 +28,7 @@ def verify_nfc(client_id, nfc_id):
     if not authorized:
         if user:
             create_log('INFO', 'Client', 'Reject', f"Unauthorized '{user.username}' at {client.name}", user, None, nfc_id)
+            current_app.logger.info(f"Unauthorized '{user.username}' at {client.name}")
         else:
             create_log('INFO', 'Client', 'Reject', f"Unknown badge '{nfc_id}' at {client.name}", None, None, nfc_id)
     else:
