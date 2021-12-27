2135d063e797087753cc96485807c85a5d61d1ab
Christian Kindel <iceman81292@gmail.com>
2021-12-26 22:23:53 -0600

    create new reset command, don't reset database when creating default admin

--- src/mcp/cli.py
+++ src/mcp/cli.py
@@ -2,6 +2,7 @@ import os
 import click
 from datetime import datetime
 import sys
+from sqlalchemy.exc import IntegrityError
 
 from flask import current_app
 from flask.cli import with_appcontext
@@ -48,72 +49,72 @@ def register(app):
     def all():
         os.system('python -W ignore::DeprecationWarning tests.py')
 
-    @test.command(cls=CLICommandInvoker)
-    def reset():
-        if os.path.exists('mcp/site.db'):
-            os.system('rm mcp/site.db')
-        if os.path.exists('migrations/'):
-            os.system('rm -r migrations/versions/*')
-        else:
-            os.system('flask db init')
-        os.system('flask db migrate')
-        os.system('flask db upgrade')
+    # @test.command(cls=CLICommandInvoker)
+    # def reset():
+    #     if os.path.exists('mcp/site.db'):
+    #         os.system('rm mcp/site.db')
+    #     if os.path.exists('migrations/'):
+    #         os.system('rm -r migrations/versions/*')
+    #     else:
+    #         os.system('flask db init')
+    #     os.system('flask db migrate')
+    #     os.system('flask db upgrade')
 
-        test_users = ['Cameron Alsop',
-                      'Leonard Black',
-                      'Trevor Wilkins',
-                      'Emily Hughes',
-                      'Steven Butler',
-                      'Samantha Clarkson',
-                      'Stephanie Allan',
-                      'Lauren Davies',
-                      'Jake Hodges',
-                      'Joe Ferguson',
-                      'Jan Lawrence',
-                      'Joe Langdon',
-                      'Benjamin Martin',
-                      'Alan Greene',
-                      'Lauren Burgess',
-                      'Ian Avery',
-                      'Dominic Powell',
-                      'Anthony North',
-                      'James Gibson',
-                      'Joshua Simpson', ]
+    #     test_users = ['Cameron Alsop',
+    #                   'Leonard Black',
+    #                   'Trevor Wilkins',
+    #                   'Emily Hughes',
+    #                   'Steven Butler',
+    #                   'Samantha Clarkson',
+    #                   'Stephanie Allan',
+    #                   'Lauren Davies',
+    #                   'Jake Hodges',
+    #                   'Joe Ferguson',
+    #                   'Jan Lawrence',
+    #                   'Joe Langdon',
+    #                   'Benjamin Martin',
+    #                   'Alan Greene',
+    #                   'Lauren Burgess',
+    #                   'Ian Avery',
+    #                   'Dominic Powell',
+    #                   'Anthony North',
+    #                   'James Gibson',
+    #                   'Joshua Simpson', ]
 
-        test_groups = ['Admininistrators',
-                       'Members',
-                       'Non-Members']
+    #     test_groups = ['Admininistrators',
+    #                    'Members',
+    #                    'Non-Members']
 
-        for user in test_users:
-            fn = user.split(' ')[0]
-            ln = user.split(' ')[1]
-            e = f"testuser+{fn}.{ln}@makeict.org"
-            u = User(first_name=fn, last_name=ln, email=e, username=f"{fn}.{ln}",
-                     email_confirmed_at=datetime.utcnow())
-            u.set_password('password')
-            db.session.add(u)
+    #     for user in test_users:
+    #         fn = user.split(' ')[0]
+    #         ln = user.split(' ')[1]
+    #         e = f"testuser+{fn}.{ln}@makeict.org"
+    #         u = User(first_name=fn, last_name=ln, email=e, username=f"{fn}.{ln}",
+    #                  email_confirmed_at=datetime.utcnow())
+    #         u.set_password('password')
+    #         db.session.add(u)
 
-        for group in test_groups:
-            g = Group(name=group, description='This is a totally awesome group description!')
-            db.session.add(g)
-            db.session.commit()
+    #     for group in test_groups:
+    #         g = Group(name=group, description='This is a totally awesome group description!')
+    #         db.session.add(g)
+    #         db.session.commit()
 
-        user = User(first_name='testy', last_name='mctestface',
-                    email='testuser+testy@makeict.org',  username='user',
-                    email_confirmed_at=datetime.utcnow())
-        user.set_password('user')
-        db.session.add(user)
+    #     user = User(first_name='testy', last_name='mctestface',
+    #                 email='testuser+testy@makeict.org',  username='user',
+    #                 email_confirmed_at=datetime.utcnow())
+    #     user.set_password('user')
+    #     db.session.add(user)
 
-        admin_role = Role(name='admin')
-        db.session.add(admin_role)
+    #     admin_role = Role(name='admin')
+    #     db.session.add(admin_role)
 
-        admin = User(first_name="Ryan", last_name="Fisher",
-                     email="testuser+admin@makeict.org", username="admin",
-                     email_confirmed_at=datetime.utcnow(), roles=[admin_role])
-        admin.set_password('admin')
-        db.session.add(admin)
+    #     admin = User(first_name="Ryan", last_name="Fisher",
+    #                  email="testuser+admin@makeict.org", username="admin",
+    #                  email_confirmed_at=datetime.utcnow(), roles=[admin_role])
+    #     admin.set_password('admin')
+    #     db.session.add(admin)
 
-        db.session.commit()
+    #     db.session.commit()
 
     @app.cli.group()
     def setup():
@@ -121,10 +122,16 @@ def register(app):
         pass
 
     @setup.command()
-    def default():
-        os.system('flask db migrate')
+    def reset():
+        db.drop_all()
         os.system('flask db upgrade')
-        
+        db.create_all()
+
+    @setup.command()
+    def default():
+        os.system('flask db upgrade')
+        db.create_all()
+
         admin_role = Role(name='admin')
         db.session.add(admin_role)
 
@@ -133,5 +140,7 @@ def register(app):
                      email_confirmed_at=datetime.utcnow(), roles=[admin_role])
         admin.set_password('admin')
         db.session.add(admin)
-
-        db.session.commit()
+        try:
+            db.session.commit()
+        except IntegrityError:
+            print("Default admin already exists!")
